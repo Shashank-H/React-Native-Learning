a97989adbb845251a77b9dc03df89d4a7e0af721
Shashank-H <shashankharikripa@gmail.com>
2020-12-02 17:57:12 +0530

    Layouting in React Native.

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,22 +5,46 @@ import { Text, StyleSheet,Button } from "react-native";
 const BoxScreen = (props) => {
 
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>text</Text>
+    <View style={styles.parentStyle}>
+      <View style={styles.view1Style}></View>
+      <View style={styles.view2Style}></View>
+      <View style={styles.view3Style}></View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle:{
+  parentStyle:{
     borderWidth:1,
     borderColor:"black",
+    // alignItems:'flex-start',
+    flexDirection:"row",
+    justifyContent:"space-around",
+    height:300,
   },
-  textStyle:{
+  view3Style:{
     borderWidth:1,
     borderColor:"red",
-    margin:2,
-  }
+    backgroundColor:"red",
+    height:50,
+    width:50,
+  },
+  view1Style:{
+    borderWidth:1,
+    borderColor:"red",
+    backgroundColor:"green",
+    height:50,
+    width:50,
+  },
+  view2Style:{
+    borderWidth:1,
+    borderColor:"red",
+    backgroundColor:"blue",
+    top:50,
+    height:50,
+    width:50,
+  },
+
 });
 
 export default BoxScreen;
